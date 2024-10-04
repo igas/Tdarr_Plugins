f055346458a20ebea29e88c37fda64d3df84371e
Igor Kapkov <github@igas.me>
2024-10-04 10:44:19 +1000

    feat: add ffmpegCommandKeepSingleVideoTrack flow plugin

---
+++ FlowPlugins/CommunityFlowPlugins/ffmpegCommand/ffmpegCommandKeepSingleVideoTrack/1.0.0/index.js
@@ -0,0 +1,51 @@
+"use strict";
+/* eslint no-plusplus: ["error", { "allowForLoopAfterthoughts": true }] */
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.plugin = exports.details = void 0;
+var flowUtils_1 = require("../../../../FlowHelpers/1.0.0/interfaces/flowUtils");
+/* eslint-disable no-param-reassign */
+var details = function () { return ({
+    name: 'Keep single audio track',
+    description: 'Remove additional audio tracks',
+    style: {
+        borderColor: 'DeepPink',
+    },
+    tags: 'audio',
+    isStartPlugin: false,
+    pType: '',
+    requiresVersion: '2.11.01',
+    sidebarPosition: -1,
+    icon: '',
+    inputs: [],
+    outputs: [
+        {
+            number: 1,
+            tooltip: 'Continue to next plugin',
+        },
+    ],
+}); };
+exports.details = details;
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+var plugin = function (args) {
+    var lib = require('../../../../../methods/lib')();
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars,no-param-reassign
+    args.inputs = lib.loadDefaultValues(args.inputs, details);
+    (0, flowUtils_1.checkFfmpegCommandInit)(args);
+    var foundTrack = false;
+    args.variables.ffmpegCommand.streams.forEach(function (stream) {
+        if (stream.codec_type === 'video') {
+            if (foundTrack) {
+                stream.removed = true;
+            }
+            else {
+                foundTrack = true;
+            }
+        }
+    });
+    return {
+        outputFileObj: args.inputFileObj,
+        outputNumber: 1,
+        variables: args.variables,
+    };
+};
+exports.plugin = plugin;
---
+++ FlowPluginsTs/CommunityFlowPlugins/ffmpegCommand/ffmpegCommandKeepSingleVideoTrack/1.0.0/index.ts
@@ -0,0 +1,58 @@
+/* eslint no-plusplus: ["error", { "allowForLoopAfterthoughts": true }] */
+
+import { checkFfmpegCommandInit } from '../../../../FlowHelpers/1.0.0/interfaces/flowUtils';
+import {
+  IpluginDetails,
+  IpluginInputArgs,
+  IpluginOutputArgs,
+} from '../../../../FlowHelpers/1.0.0/interfaces/interfaces';
+
+/* eslint-disable no-param-reassign */
+const details = (): IpluginDetails => ({
+  name: 'Keep single audio track',
+  description: 'Remove additional audio tracks',
+  style: {
+    borderColor: 'DeepPink',
+  },
+  tags: 'audio',
+  isStartPlugin: false,
+  pType: '',
+  requiresVersion: '2.11.01',
+  sidebarPosition: -1,
+  icon: '',
+  inputs: [],
+  outputs: [
+    {
+      number: 1,
+      tooltip: 'Continue to next plugin',
+    },
+  ],
+});
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const plugin = (args: IpluginInputArgs): IpluginOutputArgs => {
+  const lib = require('../../../../../methods/lib')();
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars,no-param-reassign
+  args.inputs = lib.loadDefaultValues(args.inputs, details);
+
+  checkFfmpegCommandInit(args);
+
+  let foundTrack = false;
+
+  args.variables.ffmpegCommand.streams.forEach((stream) => {
+    if (stream.codec_type === 'video') {
+      if (foundTrack) {
+        stream.removed = true;
+      } else {
+        foundTrack = true;
+      }
+    }
+  });
+
+  return {
+    outputFileObj: args.inputFileObj,
+    outputNumber: 1,
+    variables: args.variables,
+  };
+};
+export { details, plugin };
